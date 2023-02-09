479856260535a497669ea113f628c984b621ce6e
aydingokce <aydingokce@vt.edu>
2023-02-09 11:17:14 -0500

    should work, untested

--- sum_array.c
+++ sum_array.c
@@ -22,26 +22,34 @@ int sum_array(int * int_array, int array_len, int n_threads) {
     int sum = 0;
 
     int* results_array = calloc(n_threads, sizeof(int));
+    pthread_t* threads = malloc(n_threads * sizeof(pthread_t));
     int indices_per_thread = ceil_divide(array_len, n_threads);
 
     for (i = 0; i < n_threads; i++) {
         sum_job* job = malloc(sizeof(sum_job));
-        job->int_array = int_array;
+        job->int_array = int_array + i * indices_per_thread;
         job->thread_num = i;
-        job->num_to_sum = min(array_len - i * indices_per_thread, indices_per_thread);
+        job->num_to_sum = min(array_len - (i * indices_per_thread), indices_per_thread);
+        job->results_array = results_array;
 
+        pthread_t new_thread;
+        threads[i] = new_thread;
+        pthread_create(threads[i], NULL, sum_thread, job);
     }
 
     for (i = 0; i < n_threads; i++) {
-
+        pthread_join(threads[i], NULL);
     }
 
-
+    for(int i = 0; i < n_threads; i++) {
+        sum += results_array[i];
+    }
 
     return sum;
 }
 
-int sum_thread(sum_job* job) {
+void* sum_thread(void* job_uncast) {
+    sum_job* job = (sum_job*)job_uncast;
     for(int i = 0; i < job->num_to_sum; i++) {
         job->results_array[job->thread_num] += job->int_array[i];
     }
