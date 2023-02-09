36489270bbd8fbc54d0ddb4be6677085c9007a1c
aydingokce <aydingokce@vt.edu>
2023-02-09 13:30:44 -0500

    multithreaded array summation

--- sum_array.c
+++ sum_array.c
@@ -17,24 +17,29 @@
         (_a + _b - 1) / _b; \
     })
 
+/**
+ * @brief sums the values of an array by splitting the task among a specified number of threads
+ * 
+ * @param int_array pointer to the integer array
+ * @param array_len length of the dynamically allocated array
+ * @param n_threads number of threads to split the addition task
+ * @return int sum of the integers in the array
+ */
 int sum_array(int * int_array, int array_len, int n_threads) {
     int i;
     int sum = 0;
 
     int* results_array = calloc(n_threads, sizeof(int));
-    pthread_t* threads = malloc(n_threads * sizeof(pthread_t));
+    pthread_t threads[n_threads];
     int indices_per_thread = ceil_divide(array_len, n_threads);
 
     for (i = 0; i < n_threads; i++) {
         sum_job* job = malloc(sizeof(sum_job));
-        job->int_array = int_array + i * indices_per_thread;
+        job->int_array = int_array + (i * indices_per_thread);
         job->thread_num = i;
         job->num_to_sum = min(array_len - (i * indices_per_thread), indices_per_thread);
         job->results_array = results_array;
-
-        pthread_t new_thread;
-        threads[i] = new_thread;
-        pthread_create(threads[i], NULL, sum_thread, job);
+        pthread_create(&threads[i], NULL, sum_thread, job);
     }
 
     for (i = 0; i < n_threads; i++) {
@@ -45,12 +50,21 @@ int sum_array(int * int_array, int array_len, int n_threads) {
         sum += results_array[i];
     }
 
+    free(results_array);
     return sum;
 }
 
+/**
+ * @brief helper function which sums up a subsection of the array
+ * 
+ * @param job_uncast sum_job struct which contains the instructions for the job
+ */
 void* sum_thread(void* job_uncast) {
     sum_job* job = (sum_job*)job_uncast;
+    int subresult = 0;
     for(int i = 0; i < job->num_to_sum; i++) {
-        job->results_array[job->thread_num] += job->int_array[i];
+        subresult += job->int_array[i];
     }
+    job->results_array[job->thread_num] = subresult;
+    free(job_uncast);
 }
--- sum_array.h
+++ sum_array.h
@@ -1,6 +1,13 @@
+/**
+ * @brief pass this struct to each thread in the array summation function.
+ * 
+ */
 typedef struct sum_job {
-    int* int_array;
-    int num_to_sum;
-    int thread_num;
-    int* results_array;
+    int* int_array;     // pointer to start of subsection of array to sum
+    int num_to_sum;     // how many indices to sum from the offset
+    int thread_num;     // index of the called thread, to store results
+    int* results_array; // pointer to the array for storing results of individual threads
 } sum_job;
+
+int sum_array(int * int_array, int array_len, int n_threads);
+void* sum_thread(void* job_uncast);
